a854cb99324d98b500eb5e5f22bea2b5b6fb7eea
Abhishek Singh <singh.msoe@gmail.com>
2023-11-24 10:01:32 +0530

    first commit

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
+
+app = FastAPI()
+templates = Jinja2Templates(directory=os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "templates"))
+
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
