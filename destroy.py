82650646af1943fdc03c14044ddfb6f52eed970c
drednor <singh.msoe@gmail.com>
2023-12-15 08:18:56 +0530

    added terraform init in destroy.py

--- destroy.py
+++ destroy.py
@@ -2,6 +2,7 @@ import subprocess
 
 def destroy_terraform():
     try:
+        subprocess.run(["terraform", "init"], check=True)
         subprocess.run(["terraform","destroy","-auto-approve"],check=True)
     except subprocess.CalledProcessError as e:
         print(f"terraform destroy failed - {e}")
