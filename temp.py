a922402388a70e71f6e6b6d7fb3138b9d72b40cf
drednor <singh.msoe@gmail.com>
2023-12-16 19:37:32 +0530

    deleted temp files

--- temp.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-  print(sys.argv)
-  if len(sys.argv) > 1:
-    if sys.argv[1] == "deploy":
-      print("deploy")
-    elif sys.argv[1] == "destroy":
-      print("destroy")
